c757eb822d0475b172e1bc2a59edf49438de1f15
Yasya Shevchenko <shevchenochkayn@gmail.com>
2021-04-15 21:04:26 +0300

    Добавлен проект

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  $(".header-burger").click(function(){
+        $(this).toggleClass('header-burger-active');
+         $(".menu-mob-wrapper").toggleClass("active");
+       });
+    
+
+});
